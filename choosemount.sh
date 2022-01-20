12798e5057ce1ec0c9e8db089d1c0e8e654a240c
Lukas Neugebauer <l.neugebauer@uke.de>
2022-01-20 13:27:51 +0100

    fixed logical error in order of statements

--- choosemount.sh
+++ choosemount.sh
@@ -4,9 +4,9 @@
 
 #ask which drive to mount. it's looking for all sd[b-z][0-9] drives that are not mounted or swap
 drive=$(lsblk -i | grep 'sd[b-z][0-9]' | egrep -v '/|SWAP' | sed 's/^[^s]\+//' | awk '{printf "%s: %s\n", $1, $4}' | dmenu -i -p "Mount which drive?")
+[[ -z $drive ]] && exit 0
 drive="/dev/${drive%:*}"
 #if none is given, exit
-[[ -z $drive ]] && exit 0
 
 #ask for the mountingpoint
 mountpoint=/mnt/$(ls /mnt | dmenu -i -p "Choose mountpoint")
