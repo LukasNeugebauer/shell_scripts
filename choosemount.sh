f5fc7497033c03d921c39393f2bcd61fe291eb8c
Lukas Neugebauer <l.neugebauer@uke.de>
2022-01-20 13:24:32 +0100

    only shows unmounted drives now

--- choosemount.sh
+++ choosemount.sh
@@ -2,8 +2,8 @@
 #
 #Bash script using dmenu to choose a hard drive and location for mounting
 
-#ask which drive to mount. it's looking for all sd[a-z][0-9] drives
-drive=$(lsblk -i | grep 'sd[a-z][0-9]' | tr '`-' ' ' | sed 's/\s\+/ /g' | awk '{printf "%s: %s\n", $1, $4}' | dmenu -i -p "Mount which drive?")
+#ask which drive to mount. it's looking for all sd[b-z][0-9] drives that are not mounted or swap
+drive=$(lsblk -i | grep 'sd[b-z][0-9]' | egrep -v '/|SWAP' | sed 's/^[^s]\+//' | awk '{printf "%s: %s\n", $1, $4}' | dmenu -i -p "Mount which drive?")
 drive="/dev/${drive%:*}"
 #if none is given, exit
 [[ -z $drive ]] && exit 0
@@ -16,4 +16,4 @@ mountpoint=/mnt/$(ls /mnt | dmenu -i -p "Choose mountpoint")
 
 #mount hardrive with user ownership
 sudo mount -o uid=$UID,gid=1000 $drive $mountpoint && notify-send "Mounted $drive to $mountpoint" && exit
-notify-send -u critical "Couldn't mount
+notify-send -u critical "Couldn't mount drive" && exit 1
